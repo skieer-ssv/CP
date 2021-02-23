cb10e9d2d1d1bca5fd5389d49ce6cb9ddff4284e
Siddhant <55557885+skieer-ssv@users.noreply.github.com>
2021-02-23 10:09:48 +0530

    Create session0001.cpp

---
+++ session0001.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//*******cout
+// int main(){
+// 	cout<<"Hello"<<'\n'<<"Sid!"; // endl moves cursor to next line & flushes out the contents to the console
+// 	return 0;
+// }
+ 
+int main(){
+//*******cin
+// int n;
+// cin>>n;
+// cout<<n+10;	
+
+//*****char
+// char c='A';// can only recognise upto 255 symbols
+// cout<<c;
+	// '' represent char but the "" represent string
+
+// char ca[]="Helllo!";
+// cout<<ca;
+
+//*******wchar
+// wchar_t w='A'; //65 is ascii values of A //used for languages such as hindi japanese
+// wcout<<w;
+	// wchar_t wa[]=L"Jello!";
+	// wcout<<wa;
+
+//*******boolean
+// bool b; //false and 0
+// cout<<b;
+
+// float-->4bytes
+// double -->8bytes
+
+//*******overflow
+// double a= 3.0/2;// int of 1.5 -->1
+// cout<<a;
+//Calculations are always performed in higher datatypes
+//double>float> long long int >long int >int >char
+
+// 10^-9<int<10^9
+// 10^-11<long int<10^11
+// 10^-18<long long int<10^18
+
+// long long int n = 1LL*100000*1000000;
+
+// unsigned int x=-10; // andy (-100 to 100) andy c=-100 -99 -98 overflow ...200-->0 100 overflow
+// cout<<x;
+	
+
+}
+
+
